a9fee8200f98d9b442efc8a86101eab4b0e7bcd8
BugHunter-X <askzenai@gmail.com>
2025-02-23 12:04:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,8 @@
+function result = myFunction(input)
+  % Some code here...
+  if input < 0
+    result = -1; % Handle negative input
+    return; % This return statement is missing in the original code 
+  end
+  % ... rest of the function
+end
